a39977a45342f16f951f8b912721503d959637b5
Unibik21 <utkarshcha21@gmail.com>
2026-02-13 15:33:34 +0530

    DP

---
+++ DP/LongestCommonSubString.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int solve(int i,int j,string s1,string s2,vector<vector<int>>&dp){
+    if(i>=s1.size() || j>=s2.size()){
+        return 0;
+    }
+    if(dp[i][j]!=-1) return dp[i][j];
+    if(s1[i]==s2[j]){
+        return dp[i][j]=1+solve(i+1,j+1,s1,s2,dp);
+    }
+    return dp[i][j]=0;
+    
+}
+
+int main(){
+
+}
---
+++ DP/PrintLCS.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+    string s1;
+    string s2;
+    cin>>s1>>s2;
+
+    vector<vector<int>>dp(s1.size()+1,vector<int>(s2.size()+1,0));
+
+    for(int i=0;i<s1.size();i++){
+        dp[i][0]=0;
+        dp[0][i]=0;
+    }
+
+    for(int i=1;i<s1.size()+1;i++){
+        for(int j=1;j<s2.size()+1;j++){
+            if(s1[i-1]==s2[j-1]){
+                dp[i][j]=1+dp[i-1][j-1]; 
+            }
+            else{
+                dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+            }
+        }
+    }
+    cout<<dp[s1.size()][s2.size()]<<endl;
+
+
+    int i = s1.size();
+    int j = s2.size();
+    string ans ="";
+    int len = dp[i][j];
+    for(int i=0;i<len;i++){
+        ans+='%';
+    }
+    while(i>0 && j>0){
+        if(s1[i-1]==s2[j-1]){
+            ans[len-1]=s1[i-1];
+            len--;
+            i--;
+            j--;
+        }
+        else if(dp[i-1][j]>dp[i][j-1]){
+            i--;
+        }
+        else{
+            j--;
+        }
+    }
+
+    cout<<ans<<endl;
+}
