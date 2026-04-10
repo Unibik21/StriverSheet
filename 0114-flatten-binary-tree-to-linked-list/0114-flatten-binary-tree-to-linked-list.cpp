0abb9d36076b316fe79c10db72077d499d668755
Utkarsh Chauhan <utkarshcha21@gmail.com>
2026-04-11 01:50:10 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.cpp
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.cpp
@@ -11,15 +11,18 @@
  */
 class Solution {
 public:
-    TreeNode* prev= NULL;
     void flatten(TreeNode* root) {
         if(root==NULL)return;
+       stack<TreeNode*>st;
+       st.push(root);
+       while(!st.empty()){
+        TreeNode* curr = st.top();
+        st.pop();
+        if(curr->right)st.push(curr->right);
+        if(curr->left)st.push(curr->left);
 
-        flatten(root->right);
-        flatten(root->left);
-
-        root->right = prev;
-        root->left = NULL;
-        prev = root;
+        if(!st.empty()) curr->right = st.top();
+        curr->left=NULL;
+       } 
     }
 };
