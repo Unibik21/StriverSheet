1e39442e6d83af4beac9fd1e511e1bad5498cbcd
Utkarsh Chauhan <utkarshcha21@gmail.com>
2026-04-11 21:26:36 +0530

    Time: 41 ms (25.89%), Space: 47.7 MB (29.74%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.cpp
+++ 0621-task-scheduler/0621-task-scheduler.cpp
@@ -1,33 +1,31 @@
 class Solution {
 public:
     int leastInterval(vector<char>& tasks, int n) {
-        unordered_map<char,int>freq;
+        unordered_map<char,int>cnt;
         for(int i=0;i<tasks.size();i++){
-            freq[tasks[i]]++;
+            cnt[tasks[i]]++;
         }
 
         priority_queue<int>pq;
-
-        for(auto &i:freq){
+        for(auto &i:cnt){
             pq.push(i.second);
         }
-        int ans =0;
+        int intv = 0;
         while(!pq.empty()){
-            int j =0;
             vector<int>temp;
+            int j =0;
             while(!pq.empty() && j<n+1){
-                int f = pq.top();
+                int m = pq.top()-1;
+                if(m!=0)temp.push_back(m);
                 pq.pop();
-                if(f-1!=0)temp.push_back(f-1);
                 j++;
-            } 
-
+            }
             for(int i=0;i<temp.size();i++){
                 pq.push(temp[i]);
             }
-            ans+= pq.empty()?j:n+1;
+            intv+= pq.empty()?j:n+1;
         }
 
-        return ans;
+        return intv;
     }
 };
