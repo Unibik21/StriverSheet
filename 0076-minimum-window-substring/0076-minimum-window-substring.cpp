da398487cd1e6d3fb4294665ad3bb143e9d800fc
Utkarsh Chauhan <utkarshcha21@gmail.com>
2026-04-18 15:12:14 +0530

    Time: 4 ms (70.02%), Space: 11.6 MB (41.11%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        int l=0;
-        int r=0;
-        int len =INT_MAX;
-        int sind = -1;
         vector<int>freq(256,0);
         for(int i=0;i<t.size();i++){
             freq[t[i]]++;
         }
 
+        int r =0;
+        int l=0;
         int cnt =0;
+        int len = INT_MAX;
+        int sind =-1;
+
         while(r<s.size()){
             if(freq[s[r]]>0)cnt++;
             freq[s[r]]--;
@@ -20,7 +21,6 @@ public:
                     len=r-l+1;
                     sind = l;
                 }
-
                 freq[s[l]]++;
                 if(freq[s[l]]>0)cnt--;
                 l++;
