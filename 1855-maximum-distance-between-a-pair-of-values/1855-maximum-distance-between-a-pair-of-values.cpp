377415a18ae6a4c8b3b69c2f4311a5248e811359
Utkarsh Chauhan <utkarshcha21@gmail.com>
2026-04-19 13:55:39 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1855-maximum-distance-between-a-pair-of-values/1855-maximum-distance-between-a-pair-of-values.cpp
+++ 1855-maximum-distance-between-a-pair-of-values/1855-maximum-distance-between-a-pair-of-values.cpp
@@ -6,7 +6,7 @@ public:
         int len = 0;
 
         while(i<nums1.size() && j<nums2.size()){
-            while(i<nums1.size() && j<nums2.size() && nums1[i]<=nums2[j]){
+            while(j<nums2.size() && nums1[i]<=nums2[j]){
                 if(i<=j)len = max(len,j-i);
                 j++;
             }
