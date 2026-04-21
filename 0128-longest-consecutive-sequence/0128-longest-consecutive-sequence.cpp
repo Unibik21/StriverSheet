976e1c2ce13b34561b66c4266f1bd193fdc9c33b
Utkarsh Chauhan <utkarshcha21@gmail.com>
2026-04-22 00:42:48 +0530

    Time: 79 ms (68.2%), Space: 88.9 MB (66.75%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -1,60 +1,20 @@
 class Solution {
 public:
-    vector<int>parent;
-    vector<int>size;
-
-    int find(int x){
-        if(parent[x]==x)return x;
-        return parent[x]=find(parent[x]);
-    }
-
-    void Union(int x,int y){
-        int dx = find(x);
-        int dy = find(y);
-        if(dx==dy)return;
-
-        if(size[dx]>size[dy]){
-            parent[dy]=dx;
-            size[dx]+=size[dy];
-        }
-        else{
-            parent[dx]=dy;
-            size[dy]+=size[dx];
-        }
-    }
-
     int longestConsecutive(vector<int>& nums) {
-        int n = nums.size();
-        if(n==0)return 0;
-        unordered_map<int,int>idx;
+        unordered_set<int>st;
 
-        parent.resize(n);
-        size.resize(n,1);
-        for(int i=0;i<n;i++){
-            parent[i]=i;
-        }
+        if(nums.size()==0)return 0;
 
         for(int i=0;i<nums.size();i++){
-            if(idx.find(nums[i])==idx.end()){
-                idx[nums[i]]=i;
-            }
+            st.insert(nums[i]);
         }
-
-        for(auto &i:idx){
-            int ind = i.second;
-            int n = i.first;
-
-            if(idx.find(n+1)!=idx.end()){
-                Union(ind,idx[n+1]);
-            }
-        }
-
         int maxi = INT_MIN;
-
-        for(int i=0;i<n;i++){
-            if(parent[i]==i){
-                maxi = max(maxi,size[i]);
-            }
+        for(auto &i:st){
+            if(st.find(i+1)!=st.end())continue;
+            int curr =1;
+            int n = i;
+            while(st.find(n-1)!=st.end()){curr++; n--;}
+            maxi = max(curr,maxi);
         }
 
         return maxi;
