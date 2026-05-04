0cc383bfff7bbcbbc3f3184004e6c710de5c4caf
Utkarsh Chauhan <utkarshcha21@gmail.com>
2026-05-04 16:25:44 +0530

    Time: 0 ms (100%), Space: 17.6 MB (18.3%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,10 +1,51 @@
 class Solution {
-public:
-    vector<int> searchRange(vector<int>& nums, int target) {
-        auto s = lower_bound(nums.begin(),nums.end(),target);
-        auto e = upper_bound(nums.begin(),nums.end(),target);
+public: 
+    int lb(vector<int>& nums, int t){  
+        int l = 0;
+        int h = nums.size()-1;
+        if(t<nums[l] || t>nums[h])return -1;
+        int ans =-1;
+        while(l<=h){
+            int m = l+(h-l)/2;
 
-        if(e-s==0)return {-1,-1};
-        return {int(s-nums.begin()),int(e-nums.begin()-1)}; 
+            if(nums[m]<=t){
+                l=m+1;
+                if(nums[m]==t){
+                    ans = m;
+                }
+            }
+            else{
+                h=m-1;
+            }
+        }
+        return ans;
+    }
+    int ub(vector<int>& nums, int t){
+        int l = 0;
+        int h = nums.size()-1;
+        if(t<nums[l] || t>nums[h])return -1;
+        int ans =-1;
+        while(l<=h){
+            int m = l+(h-l)/2;
+
+            if(nums[m]>=t){
+                h=m-1;
+                if(nums[m]==t){
+                    ans = m;
+                }
+            }
+            else{
+                l=m+1;
+            }
+        }
+
+        return ans;
+    }
+    vector<int> searchRange(vector<int>& nums, int target) {
+        if(nums.size()==0)return {-1,-1};
+        int s = ub(nums,target);
+        if(s==-1)return {-1,-1};
+        int e = lb(nums,target);
+        return {s,e};
     }
 };
