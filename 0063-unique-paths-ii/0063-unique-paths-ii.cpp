a2c31701f121f24033fe5ac134b59ae4c57321c8
Utkarsh Chauhan <utkarshcha21@gmail.com>
2026-05-23 09:10:02 +0530

    Time: 0 ms (100%), Space: 11.7 MB (77.25%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,20 +1,34 @@
 class Solution {
 public:
-    int solve(int i,int j, vector<vector<int>>&mat,vector<vector<int>>&dp){
-        if(i==mat.size()-1 && j==mat[0].size()-1)return 1;
-        if(i>mat.size()-1 || j>mat[0].size()-1)return 0;
-        if(dp[i][j]!=-1)return dp[i][j];
-        int right = 0;
-        int down= 0;
-
-        if(j<mat[0].size()-1 && mat[i][j+1]!=1)right = solve(i,j+1,mat,dp);
-        if(i<mat.size()-1 && mat[i+1][j]!=1)down = solve(i+1,j,mat,dp);
-
-        return dp[i][j]=right+down; 
-    }
     int uniquePathsWithObstacles(vector<vector<int>>& mat) {
         if(mat[0][0]==1 || mat[mat.size()-1][mat[0].size()-1]==1)return 0;
-        vector<vector<int>>dp(mat.size(),vector<int>(mat[0].size(),-1));
-        return solve(0,0,mat,dp);
+        vector<vector<long long>>dp(mat.size(),vector<long long>(mat[0].size(),0));
+        int n = mat.size();
+        int m = mat[0].size();
+
+        dp[n-1][m-1]=1;
+
+        for(int i=n-1;i>=0;i--){
+            for(int j=m-1;j>=0;j--){
+                if(i==n-1 && j==m-1)continue;
+                if(mat[i][j] == 1) {
+                    dp[i][j] = 0;
+                    continue;
+                }
+
+                long long right = 0;
+                long long down = 0;
+
+                if(j+1 < m)
+                    right = dp[i][j+1];
+
+                if(i+1 < n)
+                    down = dp[i+1][j];
+
+                dp[i][j] = right + down;
+            }
+        }
+
+        return (int)dp[0][0];
     }
 };
