7600d8c9d6403f141d796db448f25b090bbd1346
Utkarsh Chauhan <utkarshcha21@gmail.com>
2026-05-24 12:56:08 +0530

    Time: 55 ms (17.65%), Space: 53 MB (13.41%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
-    int solve(int i,int amount,vector<int>&coins,vector<vector<int>>&dp){
-        if(amount==0)return 0;
-        if(i==coins.size())return 1e9;
-        if(dp[i][amount]!=-1)return dp[i][amount];
-        int nottake = solve(i+1,amount,coins,dp);
-        int take = 1e9;
-        if(coins[i]<=amount)take = 1+solve(i,amount-coins[i],coins,dp);
-        return dp[i][amount]=min(take,nottake);
-    }
     int coinChange(vector<int>& coins, int amount){
-        vector<vector<int>>dp(coins.size(),vector<int>(amount+1,-1));
-        int total= solve(0,amount,coins,dp);
-        return total==1e9?-1:total;
+        vector<vector<int>>dp(coins.size()+1,vector<int>(amount+1,1e9));
+        for(int i=0;i<coins.size();i++)dp[i][0]=0;
+
+        for(int i=coins.size()-1;i>=0;i--){
+            for(int j=1;j<=amount;j++){
+                int nottake = dp[i+1][j];
+                int take = 1e9;
+                if(coins[i]<=j)take = 1+dp[i][j-coins[i]];
+                dp[i][j]=min(take,nottake);
+            }
+        }
+        return dp[0][amount]==1e9?-1:dp[0][amount];
     }
 };
