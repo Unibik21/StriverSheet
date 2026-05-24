4a7c76f643938b81351deeb39fb192a28e39fa51
Utkarsh Chauhan <utkarshcha21@gmail.com>
2026-05-25 05:09:03 +0530

    Time: 19 ms (45.62%), Space: 26.4 MB (20.76%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,21 +1,23 @@
 class Solution {
 public:
-    int solve(int i,int t,vector<int>&nums,vector<vector<int>>&dp,int sum){
-        if(i==nums.size()){
-            if(t==0)return 1;
-            return 0;
-        }
-        if(t<-sum || t>sum)return 0;
-        if(dp[i][t+sum]!=-1)return dp[i][t+sum];
-        int pos = solve(i+1,t-nums[i],nums,dp,sum);
-        int neg = solve(i+1,t+nums[i],nums,dp,sum);
-
-        return dp[i][t+sum]=pos+neg;
-    }
     int findTargetSumWays(vector<int>& nums, int target) {
         int sum = 0;
         for(int i=0;i<nums.size();i++)sum+=nums[i];
-        vector<vector<int>>dp(nums.size(),vector<int>(2*sum+1,-1));
-        return solve(0,target,nums,dp,sum);
+        vector<vector<int>>dp(nums.size()+1,vector<int>(2*sum+1,0));
+        if(target < -sum || target > sum)return 0;
+        int n = nums.size();
+        int m = 2*sum+1;
+        dp[n][sum]=1;
+
+        for(int i=n-1;i>=0;i--){
+            for(int t=-sum;t<=sum;t++){
+                int ways= 0;
+                if(t-nums[i]>=-sum)ways+=dp[i+1][t-nums[i]+sum];
+                if(t+nums[i]<=sum)ways+=dp[i+1][t+nums[i]+sum];
+                dp[i][t+sum]=ways;
+            }
+        }
+
+        return dp[0][target+sum];
     }
 };
