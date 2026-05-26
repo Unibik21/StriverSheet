3e9fcc88ef4788bd9a51273520b7bf0c0da0d407
Utkarsh Chauhan <utkarshcha21@gmail.com>
2026-05-26 09:46:30 +0530

    Time: 8 ms (47.61%), Space: 13.2 MB (59.05%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,22 +1,19 @@
 class Solution {
 public:
-    int solve(int i,int j,string &w1, string &w2,vector<vector<int>>&dp){
-        if(i==w1.size()){
-            return w2.size()-j;
-        }
-        if(j==w2.size()){
-            return w1.size()-i;
-        }
-        if(dp[i][j]!=-1)return dp[i][j];
-        if(w1[i]==w2[j])return dp[i][j]=solve(i+1,j+1,w1,w2,dp);
-
-        int op1 = 1+min(solve(i+1,j,w1,w2,dp),solve(i,j+1,w1,w2,dp));
-        int op2 = 1+solve(i+1,j+1,w1,w2,dp);
-
-        return dp[i][j]=min(op1,op2);
-    }
     int minDistance(string w1, string w2) {
-        vector<vector<int>>dp(w1.size(),vector<int>(w2.size(),-1));
-        return solve(0,0,w1,w2,dp);
+        vector<vector<int>>dp(w1.size()+1,vector<int>(w2.size()+1,0));
+        
+        for(int i=0;i<=w1.size();i++)dp[i][w2.size()]=w1.size()-i;
+        for(int i=0;i<=w2.size();i++)dp[w1.size()][i]=w2.size()-i;
+
+        for(int i=w1.size()-1;i>=0;i--){
+            for(int j=w2.size()-1;j>=0;j--){
+                if(w1[i]==w2[j])dp[i][j]=dp[i+1][j+1];
+                else{
+                    dp[i][j] = min({1+dp[i+1][j+1],1+dp[i+1][j],1+dp[i][j+1]});
+                }
+            }
+        }
+        return dp[0][0];
     }
 };
