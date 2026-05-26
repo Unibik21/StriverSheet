e1a6c07d8ebd8e0e940624e8ef43b2e342273c2f
Utkarsh Chauhan <utkarshcha21@gmail.com>
2026-05-26 07:10:59 +0530

    Time: 15 ms (34.01%), Space: 16.8 MB (56.44%) - LeetHub

--- 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
@@ -15,7 +15,27 @@ public:
         return dp[i][j]=1+min(solve(i+1,j,w1,w2,dp),solve(i,j+1,w1,w2,dp));
     }
     int minDistance(string w1, string w2) {
-        vector<vector<int>>dp(w1.size(),vector<int>(w2.size(),-1));
-        return solve(0,0,w1,w2,dp);
+        vector<vector<int>>dp(w1.size()+1,vector<int>(w2.size()+1,0));
+        
+
+        for(int i=0;i<=w1.size();i++){
+            dp[i][w2.size()]=w1.size()-i;
+        }
+        for(int i=0;i<=w2.size();i++){
+            dp[w1.size()][i]=w2.size()-i;
+        }
+
+        for(int i=w1.size()-1;i>=0;i--){
+            for(int j=w2.size()-1;j>=0;j--){
+                if(w1[i]==w2[j]){
+                    dp[i][j]=dp[i+1][j+1];
+                }
+                else{
+                    dp[i][j]=1+min(dp[i+1][j],dp[i][j+1]); 
+                }
+            }
+        }
+        return dp[0][0];
+
     }
 };
