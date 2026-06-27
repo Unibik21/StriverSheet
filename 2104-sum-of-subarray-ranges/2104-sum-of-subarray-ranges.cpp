50a4bcf32712ab541f1a2eb72e047e45c6d1c064
Utkarsh Chauhan <utkarshcha21@gmail.com>
2026-06-27 18:16:22 +0530

    Time: 4 ms (58.94%), Space: 15.7 MB (57.61%) - LeetHub

--- 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.cpp
+++ 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.cpp
@@ -1,17 +1,52 @@
 class Solution {
 public:
     long long subArrayRanges(vector<int>& nums) {
-       long long sum =0;
-        for(int i=0;i<nums.size();i++){
-            int maxi = nums[i];
-            int mini = nums[i];
-            for(int j=i;j<nums.size();j++){
-                maxi=max(maxi,nums[j]);
-                mini = min(mini,nums[j]);
-                sum+=(maxi-mini);
-            }
-        } 
+        vector<int>pse(nums.size());
+        vector<int>nse(nums.size());
+        vector<int>pge(nums.size());
+        vector<int>nge(nums.size());
 
+        stack<int>st;
+        for(int i=0;i<nums.size();i++){
+            while(!st.empty() && nums[st.top()]>nums[i])st.pop();
+            if(st.empty())pse[i]=-1;
+            else pse[i]=st.top();
+            st.push(i);
+        }
+        while(!st.empty())st.pop();
+        for(int i=0;i<nums.size();i++){
+            while(!st.empty() && nums[st.top()]<=nums[i])st.pop();
+            if(st.empty())pge[i]=-1;
+            else pge[i]=st.top();
+            st.push(i);
+        }
+        while(!st.empty())st.pop();
+        for(int i=nums.size()-1;i>=0;i--){
+            while(!st.empty() && nums[st.top()]>=nums[i])st.pop();
+            if(st.empty())nse[i]=nums.size();
+            else nse[i]=st.top();
+            st.push(i);
+        }
+        while(!st.empty())st.pop();
+        for(int i=nums.size()-1;i>=0;i--){
+            while(!st.empty() && nums[st.top()]<nums[i])st.pop();
+            if(st.empty())nge[i]=nums.size();
+            else nge[i]=st.top();
+            st.push(i);
+        }
+
+        long long sum =0;
+        for(int i=0;i<nums.size();i++){
+            int ls = i-pse[i];
+            int rs = nse[i]-i;
+            int ll = i-pge[i];
+            int rl = nge[i]-i;
+
+            long long s = 1LL*nums[i]*ls*rs;
+            long long l = 1LL*nums[i]*ll*rl;
+
+            sum+=l-s;
+        }
         return sum;
     }
 };
