e7dbc8392b6d9856acc81e4860808440e0398e13
Utkarsh Chauhan <utkarshcha21@gmail.com>
2026-07-25 08:43:57 +0530

    Time: 0 ms (100%), Space: 10.2 MB (5.88%) - LeetHub

--- 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.cpp
+++ 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.cpp
@@ -1,12 +1,8 @@
 class Solution {
 public:
     int maxProduct(int n) {
-        vector<int>temp;
-        while(n!=0){
-            temp.push_back(n%10);
-            n=n/10;
-        }
-        sort(temp.begin(),temp.end());
-        return temp[temp.size()-1]*temp[temp.size()-2];
+        string str = to_string(n);
+        sort(str.rbegin(),str.rend());
+        return (str[0]-'0')*(str[1]-'0');
     }
 };
