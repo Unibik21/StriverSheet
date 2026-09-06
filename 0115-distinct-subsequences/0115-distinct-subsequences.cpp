e284a43f48764c3e2cac9a8c9bf573095f320ea1
Utkarsh Chauhan <utkarshcha21@gmail.com>
2026-09-06 11:26:53 +0530

    Time: 31 ms (53.36%), Space: 44.5 MB (11.48%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -18,7 +18,25 @@ public:
         return dp[i][j]=ans;
     }
     int numDistinct(string s, string t) {
-        vector<vector<int>>dp(s.size(),vector<int>(t.size(),-1));
-        return solve(0,0,s,t,dp);
+        vector<vector<double>>dp(s.size()+1,vector<double>(t.size()+1,0));
+        // return solve(0,0,s,t,dp);
+
+        dp[s.size()][t.size()]=1;
+        for(int i=0;i<s.size();i++)dp[i][t.size()]=1;
+
+        for(int i=s.size()-1;i>=0;i--){
+            for(int j=t.size()-1;j>=0;j--){
+                double ans =0;
+                if(s[i]==t[j]){
+                    double take = dp[i+1][j+1];
+                    ans+=take;
+                }
+                double nottake = dp[i+1][j];
+                ans+=nottake;
+
+                dp[i][j]=ans;
+            }
+        }
+        return (int)dp[0][0];
     }
 };
