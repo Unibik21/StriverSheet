8b9551138a00bbc9f034b70e90e9ca2b63a8ff07
Utkarsh Chauhan <utkarshcha21@gmail.com>
2026-09-26 22:08:08 +0530

    Time: 74 ms (74.45%), Space: 122.7 MB (76.01%) - LeetHub

--- 1807-evaluate-the-bracket-pairs-of-a-string/1807-evaluate-the-bracket-pairs-of-a-string.cpp
+++ 1807-evaluate-the-bracket-pairs-of-a-string/1807-evaluate-the-bracket-pairs-of-a-string.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     string evaluate(string s, vector<vector<string>>& knowledge) {
         string ans = "";
-        map<string,string>mp;
+        unordered_map<string,string>mp;
         for(auto &i:knowledge){
             string key = i[0];
             string val = i[1];
